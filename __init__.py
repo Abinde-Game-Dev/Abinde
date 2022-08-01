c587bb780cf351e22b1fe5911ac4fd862cf9e756
desvasicek <desmond.vasicek@protonmail.com>
2022-08-01 12:45:44 -0700

    v0.0.4 :laughing:

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 print("""Pytine 0.0.2
-Pytine is an easy-to-use, open-source module for pygame. https://github.com/desvasicek/Pytine/""")
+Pytine is an easy-to-use, open-source module for pygame. | GitHub: https://github.com/desvasicek/Pytine/ | PyPI: https://pypi.org/project/Pytine/0.0.4/ |""")
