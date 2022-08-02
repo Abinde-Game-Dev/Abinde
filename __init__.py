8d007ebb3c1cc787dfa0287730306034eac3a9e9
Des <84301435+desvasicek@users.noreply.github.com>
2022-08-02 11:57:27 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-print("""Pytine 1.0
-Pytine is an easy-to-use, open-source module for pygame. | GitHub: https://github.com/desvasicek/Pytine/ | PyPI: https://pypi.org/project/Pytine/0.0.4/ |""")
+print("""Abinde 1.2
+Abinde is an easy-to-use, open-source module for pygame. | GitHub: https://github.com/desvasicek/Abinde/ | PyPI: https://pypi.org/project/Abinde/ |""")
