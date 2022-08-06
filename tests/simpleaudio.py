110ac0a78c88370fa50e2d5d81e75815d9d5d314
desvasicek <desmond.vasicek@protonmail.com>
2022-08-06 15:43:58 -0700

    ;)

--- tests/simpleaudio.py
+++ tests/simpleaudio.py
@@ -1,6 +1,6 @@
 import Abinde as ab
 game = ab.Game("Audio Test")
-text = ab.sprite.Text("Sans-Serif", "Just Listen.", 70, color=ab.color.RED)
+text = ab.sprite.Text("Sans-Serif", "Just Listen.", 70, color=ab.color.ORANGE)
 audio = ab.Audio("sample1.mp3")
 audio.play()
 game.mainloop()
--- tests/simpledrawing.py
+++ tests/simpledrawing.py
@@ -1,6 +1,7 @@
 import Abinde as ab
 game = ab.Game()
-ellipse = ab.sprite.Ellipse(color=ab.color.RED)
+ellipse = ab.sprite.Ellipse(color=ab.color.RED1)
 line = ab.sprite.Line()
-rect = ab.sprite.Rectangle(color=ab.color.PURPLE)
+rect = ab.sprite.Rectangle(color=ab.color.PURPLE1)
 game.mainloop()
+help(ab)
