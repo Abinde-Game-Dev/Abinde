1068b1ba3229891b386009f1cfd513a704540cd3
desvasicek <84301435+desvasicek@users.noreply.github.com>
2022-08-11 09:21:20 -0700

    v2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Abinde',
-    version='1.9.7',
+    version='2.0',
     install_requires=[
         'pygame>=2.1.2',
         'importlib-metadata; python_version >= "3.8"',
